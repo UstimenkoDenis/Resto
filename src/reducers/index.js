dd7c0e7e0e56297c96f22ee747af4289b0995edb
Denis Ustimenko <59954252+UstimenkoDenis@users.noreply.github.com>
2020-06-30 23:02:15 +0300

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,7 @@ const initialState = {
    loading: true,
    error: false,
    items: [],
-   total: 0         // элементы которые пользователь положил в корзину
-}
+   total: 0        
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
@@ -16,7 +15,7 @@ const reducer = (state = initialState, action) => {
                 error: false
 
             }
-        case 'MENU_REQUESTED' : // идет запрос к базе данных на меню
+        case 'MENU_REQUESTED' : 
         
             return {
                 ...state,
@@ -29,22 +28,14 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 error: true
             }
-        case 'ITEM_ADD_TO_CART': //      здесь в items нужно положить какой то элемент  который 
-//                                       уже есть в menu, вычислить его по тому id который пришел в этот action вытащить от туда
-//                                       сформировать новый объект и 
-//                                       поместить в items для дальнейшего использования в cartTable
-            
+        case 'ITEM_ADD_TO_CART': 
             const id = action.payload;
-            const item = state.menu.find(item => item.id === id );  // ищем тот самый элемент в меню
-            
-            const itemFromItems = state.items.find(item => item.id === id) //есть ли в items наша покупка?, объект записываем в itemItems
+            const item = state.menu.find(item => item.id === id );              
+            const itemFromItems = state.items.find(item => item.id === id) 
            
-            if (itemFromItems){   // если нашли то - true 
-               
-                const itemFromItemsIndex = state.items.findIndex(item => item.id === id); // определяем индекс этого продукта в items
-               
-                const newItemFromItems = { // создаем новый объект продукта с новым количеством
-
+            if (itemFromItems){                  
+                const itemFromItemsIndex = state.items.findIndex(item => item.id === id);                
+                const newItemFromItems = {
                     title: itemFromItems.title,
                     price: itemFromItems.price,
                     url: itemFromItems.url,
@@ -54,46 +45,39 @@ const reducer = (state = initialState, action) => {
                 let sum = newItemFromItems.price;
                 state.items.forEach(item =>sum += (item.price*item.numberOf));
                
-                return {                // формируем новый объект state
-                    ...state, // state свойства остальные
-                    items: [ // массив формируем из элементов до и после ,а между ними перезаписываем наш продукт с новым количеством
+                return {                
+                    ...state, 
+                    items: [ 
                         ...state.items.slice(0, itemFromItemsIndex),
                         newItemFromItems,
                         ...state.items.slice(itemFromItemsIndex + 1)
                     ],
                     total: sum
                 }
-            } else { // если itemFromItems - undefined
-                const newItem = {     // создаем новый продукт в  корзине
-
+            } else { 
+                const newItem = {    
                     title: item.title,
                     price: item.price,
                     url: item.url,
                     id: item.id,
-                    numberOf: 1     // ставим количество 1 шт
+                    numberOf: 1     
                 };
                 let sum = newItem.price;
                 state.items.forEach(item => {sum += (item.price*item.numberOf)})
                 return {
-                    ...state,  // весь state кроме items
+                    ...state,  
                     items: [
                         ...state.items,  
-                        newItem       //  и добавляем новый продукт
+                        newItem       
                     ],
                     total: sum
                 };
-            }
-           
-           
-           
-           
-          
+            }          
            
         case 'ITEM_REMOVE_FROM_CART':
             const idx = action.payload;
             const itemIndex = state.items.findIndex(item => item.id === idx);
-
-            const forDeleteTotal = state.items[itemIndex]; // - объект который будем удалять запишем в forDeleteTotal
+            const forDeleteTotal = state.items[itemIndex]; 
 
             return {
                 ...state,
@@ -101,7 +85,7 @@ const reducer = (state = initialState, action) => {
                     ...state.items.slice(0, itemIndex),
                     ...state.items.slice(itemIndex + 1)
                 ],
-                total: state.total - (forDeleteTotal.price*forDeleteTotal.numberOf) // вычитаем из общей суммы - сумму удаленных продуктов
+                total: state.total - (forDeleteTotal.price*forDeleteTotal.numberOf)
             } 
         default:
                  return state;
